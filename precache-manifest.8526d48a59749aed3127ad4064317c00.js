8059598cf0f5b0a1ab5e788f4f547e297c821b5b
unknown <Yash Kumar>
2019-10-25 10:07:31 +0530

    Updates

---
+++ precache-manifest.8526d48a59749aed3127ad4064317c00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "49feb0d5f28392f57536",
+    "url": "/SMART_HOME_ASSISTANT_PWA/static/js/runtime~main.49feb0d5.js"
+  },
+  {
+    "revision": "a4bc7d00a48063ad679f",
+    "url": "/SMART_HOME_ASSISTANT_PWA/static/js/main.a4bc7d00.chunk.js"
+  },
+  {
+    "revision": "19df992304d0cfc9441b",
+    "url": "/SMART_HOME_ASSISTANT_PWA/static/js/1.19df9923.chunk.js"
+  },
+  {
+    "revision": "a4bc7d00a48063ad679f",
+    "url": "/SMART_HOME_ASSISTANT_PWA/static/css/main.885be257.chunk.css"
+  },
+  {
+    "revision": "2e980a51fcd955063f9fb065e70f71fc",
+    "url": "/SMART_HOME_ASSISTANT_PWA/index.html"
+  }
+];
